5742fb664a6e51d5339b661a6f746688e3b7488d
Era Muha <142845027+era-emuha@users.noreply.github.com>
2025-04-06 15:36:09 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-document.querySelectorAll("button").addEventListener("click", handleClick);
+let nOfDrums = document.querySelectorAll(".drum").length
 
-function handleClick() {
-    alert("Hello");
-  }
+for (let index = 0; index < nOfDrums; index++) {
+    document.querySelectorAll(".drum")[index].addEventListener("click", handleClick);
+
+    function handleClick() {
+        alert("Hello");
+    }
+
+}
