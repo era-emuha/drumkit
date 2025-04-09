efeb0d06119f049e6a52d434c3ac3ef4d1accd1e
Era Muha <142845027+era-emuha@users.noreply.github.com>
2025-04-09 18:24:57 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let drumButtons = document.querySelectorAll(".drum");
 
+// handle mouse click
 function handleClick() {
     let soundDrum = this.innerHTML;
 
@@ -40,4 +41,48 @@ function handleClick() {
 
 for (let i = 0; i < drumButtons.length; i++) {
     drumButtons[i].addEventListener("click", handleClick);
+}
+
+
+// handle keyboard press
+function handleKey() {
+    let soundDrum = this.innerHTML;
+
+    switch (soundDrum) {
+        case "w":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            let kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+    
+        default:
+            break;
+    }
+}
+
+for (let i = 0; i < drumButtons.length; i++) {
+    drumButtons[i].addEventListener("keypress", handleKey);
 }
